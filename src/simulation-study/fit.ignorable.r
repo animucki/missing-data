792cb86e4886f7055e3fb9fcdcb57e611b9c25a0
Bartosz Animucki <b.animucki@gmail.com>
2020-01-29 15:41:17 +0100

    Implement ignorable model

---
+++ src/simulation-study/fit.ignorable.r
@@ -0,0 +1,22 @@
+fit.ignorable <- function(d, key) {
+  flog.debug(paste0('Fitting ignorable model to sample ', key))
+  
+  m <- lmer(y ~ (1|subject) + treatment + time,
+            data=d, REML=F)
+  beta <- fixef(m)
+  names(beta)[1] <- 'intercept'
+  
+  seFixed <- diag(vcov(m))
+  names(seFixed) <- paste0('se.',names(beta))
+  
+  #extract Wald standard errors for variance components (not included in output by default)
+  dd.ML <- lme4:::devfun2(m, useSc=TRUE, signames=FALSE)
+  vv <- as.data.frame(VarCorr(m))
+  pars <- vv$sdcor
+  names(pars) <- c('sigma.b','sigma')
+  hh <- hessian(dd.ML, pars)
+  vv <- diag(2*solve(hh))
+  names(vv) <- c('se.sigma.b','se.sigma')
+  
+  as.data.frame(as.list(c(beta, pars, seFixed, vv)))
+}
--- src/simulation-study/main.r
+++ src/simulation-study/main.r
@@ -4,6 +4,7 @@ rm(list = ls())
 flog.info('Loading libraries...')
 library(dplyr)
 library(futile.logger)
+library(lme4)
 library(mvtnorm)
 library(numDeriv)
 library(stats4)
@@ -11,9 +12,16 @@ library(tidyr)
 
 flog.info('Sourcing functions...')
 source("src/simulation-study/generateSamples.r")
+source("src/simulation-study/fit.ignorable.r")
 
-
-flog.threshold('info')
+flog.threshold('debug')
 df <- generateSamples(samples = 10)
 
 # View(df)
+
+## ONE SAMPLE
+d <- df %>% filter(sample == 1) %>% mutate(y=yMAR)
+fit.ignorable(d, 1)
+
+# ALL SAMPLES
+res <- df %>% mutate(y=yMAR) %>% group_by(sample) %>% group_modify(fit.ignorable)
