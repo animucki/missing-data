db219af23c8f39342636a5537c1d172d6a20c8d9
Bartosz Animucki <b.animucki@gmail.com>
2020-02-18 21:03:56 +0100

    Test class model

--- src/simulation-study/main.r
+++ src/simulation-study/main.r
@@ -21,23 +21,21 @@ source("src/simulation-study/generateSamples.r")
 source("src/simulation-study/fit.ignorable.r")
 source("src/simulation-study/fit.parametric.r")
 source("src/simulation-study/fit.hybrid.r")
+source("src/simulation-study/fit.class.r")
 
 set.seed(666L)
-df1 <- generateSamples(samples = 1, participants = 10000)
+df1 <- generateSamples(samples = 1, participants = 1000)
 
 # ALL SAMPLES
 res <- list()
 
-flog.info('Testing parametric method with analytic derivatives...')
-# res[[1]] <- df1 %>% mutate(y=yMAR, r=rMAR) %>% group_split(sample) %>% lapply(fit.parametric) %>% bind_rows
+flog.info('Testing class model...')
+res[[1]] <- df1 %>% mutate(y=yMAR, r=rMAR) %>% group_split(sample) %>% lapply(fit.class) %>% bind_rows
 
-flog.info('Testing hybrid method with analytic derivatives...')
-res[[2]] <- df1 %>% mutate(y=yMAR, r=rMAR) %>% group_split(sample) %>% lapply(fit.hybrid) %>% bind_rows
-
-flog.info('Fitting models to MAR scenario...')
+# flog.info('Fitting models to MAR scenario...')
 # res[[1]] <- df1 %>% mutate(y=yMAR, r=rMAR) %>% group_split(sample) %>% mclapply(fit.multiple) %>% bind_rows %>% mutate(scenario='MAR')
 
-flog.info('Fitting models to MNAR scenario...')
+# flog.info('Fitting models to MNAR scenario...')
 # res[[2]] <- df1 %>% mutate(y=yMNAR, r=rMNAR) %>% group_split(sample) %>% mclapply(fit.multiple) %>% bind_rows %>% mutate(scenario='MNAR')
 
 result <- bind_rows(res)
