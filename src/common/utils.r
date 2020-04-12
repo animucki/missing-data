5ee4cd348a34610a4837af52a85d3d5039429013
Bartosz <Animucki>
2020-04-12 22:04:26 +0200

    Fix softmax NaN

--- src/common/utils.r
+++ src/common/utils.r
@@ -1,6 +1,11 @@
 #' Calculate the multinomial logit prediction function
 softmax <- function(x) {
-  exp(x) / sum(exp(x))
+  t <- exp(x) / sum(exp(x))
+  if(any(!is.finite(t))) {
+    return(as.numeric(x==max(x)))
+  } else {
+    return(t)
+  }
 }
 
 #' matrix trace
